d4e5e4981f6457eb459c2c018646aa47bc1ccb78
Angelique Thevenet <a.thevenet3008@gmail.com>
2026-08-28 13:51:50 +0200

    refactor: renommage colonnes odisse canicule (departement, pas commune)

--- models/staging/stg_odisse_canicule.sql
+++ models/staging/stg_odisse_canicule.sql
@@ -1,18 +1,17 @@
 {{ config(
     materialized = 'table',
-    description = 'Données sanitaires Odissé nettoyées',
     tags = ['staging', 'health']
 ) }}
 
 SELECT
   annee,
-  libgeo AS commune_name,
-  dep AS dept_code,
+  libgeo AS departement_nom,
+  dep AS departement_code,
   reg,
   nom_region,
-  nb_j_can,
+  nb_j_can AS nb_jours_canicule,
   CURRENT_TIMESTAMP() AS loaded_at
 FROM {{ source('raw', 'raw_odisse_canicule') }}
-WHERE annee IS NOT NULL 
+WHERE annee IS NOT NULL
   AND libgeo IS NOT NULL
   AND nb_j_can IS NOT NULL
